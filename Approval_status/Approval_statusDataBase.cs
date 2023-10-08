407c86d2aba56228adb07fa263162ae5ffc930cb
Alexandr <a.a.shapovov@bk.ru>
2023-10-08 11:48:02 +0300

    Performed work in Approval_status,Added new windows, it remains to finalize adding data to SQL

---
+++ Approval_status/Approval_statusDataBase.cs
@@ -0,0 +1,25 @@
+﻿using ARM_Engineer.Database;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Documents;
+
+namespace ARM_Engineer.Approval_status
+{
+    class Approval_statusDataBase
+    {
+        
+        public void Approval_statusDataBaseTable()
+        {
+         
+            
+                    
+                
+            
+        }
+    }
+}
---
+++ Approval_status/Approval_status_add.xaml.cs
@@ -0,0 +1,59 @@
+﻿using ARM_Engineer.Database;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace ARM_Engineer.Approval_status
+{
+    /// <summary>
+    /// Логика взаимодействия для Approval_status_add.xaml
+    /// </summary>
+    public partial class Approval_status_add : Window
+    {
+        public Approval_status_add()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Save_Approval_status_Click(object sender, RoutedEventArgs e)
+        {
+           
+
+
+            //cmd.Parameters.Add(new NpgsqlParameter("@Name", textBox_Name_Approval_status.Text));
+            //cmd.Parameters.Add(new NpgsqlParameter("@Description", textBox_Name_Approval_status.Text));
+            //cmd.ExecuteNonQuery();
+            if (textBox_Name_Approval_status.Text != "" & textBox_Name_Approval_status.Text != "")
+            {
+                try
+                {
+                    NpgsqlCommand cmd = new NpgsqlCommand("insert into public.\"Approval_status\"(name,description) values (" + textBox_Name_Approval_status.Text + ",'" + textBox_Name_Approval_status.Text + "')", DataBaseConnection.Connection());
+                    cmd.ExecuteNonQuery();
+
+                    MessageBox.Show("Record inserted ");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, ex.GetType().ToString(), MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+
+
+
+            }
+
+             
+        }
+    }
+}
