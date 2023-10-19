3293f4f672d8a0a70e3f01817eb0219d98d87be3
Alexandr Shapovalov <a.a.shapovalov@bk.ru>
2023-10-19 07:10:32 +0300

    fixed error

--- Employee/EmployeeAddChangeWindow.xaml.cs
+++ Employee/EmployeeAddChangeWindow.xaml.cs
@@ -35,15 +35,12 @@ namespace ARM_Engineer.Employee
                 Output_last_name_first_name_patronymic_Label.Content = model.Name;
                 Table_number_TextBox.Text = model.Service_number;
                 Name_TextBox.Text = model.Name;
-                // Division_Combobox.Text = model.ID_Division.ToString();
                 Date_employment_DatePicker.SelectedDate = model.DataEmployee;
                 Date_deletion_DatePicker.SelectedDate = model.DateDismissial;
-                //  Post_Combobox.Text = model.ID_Post.ToString();
-                textBoxOrganization.Text = model.Name;
-                textBoxPost.Text = model.Name;
-                textBoxDivision.Text = model.Name;
-
-
+                DateYearBirth_employment_DatePicker.SelectedDate = model.YearOfBirth;
+                textBoxOrganization.Text = model.Organization.Name;
+                textBoxPost.Text = model.Post.Name;
+                textBoxDivision.Text = model.Division.Name;
             }
         }
         void ChangeAndEmployee()
@@ -52,20 +49,17 @@ namespace ARM_Engineer.Employee
             {
                 try
                 {
-                    NpgsqlCommand cmd = new NpgsqlCommand("insert into \"Employee\" (\"service_number\", \"name\",\"date_employee\",\"date_dismissal\",\"id_organization\",\"id_division\",\"id_post\" ) " +
-                                                          "values(@service_number,@name,@date_employee,@date_dismissal,@id_organization,@id_division,@id_post)",
+                    NpgsqlCommand cmd = new NpgsqlCommand("insert into \"Employee\" (\"service_number\", \"name\",\"date_employee\",\"date_dismissal\",\"id_organization\",\"year_birth\",\"id_division\",\"id_post\" ) " +
+                                                          "values(@service_number,@name,@date_employee,@date_dismissal,@id_organization,@year_birth,@id_division,@id_post)",
                                                           DataBase.Connection());
                     cmd.Parameters.Add(new NpgsqlParameter("@service_number", Table_number_TextBox.Text));
                     cmd.Parameters.Add(new NpgsqlParameter("@name", Name_TextBox.Text));
                     cmd.Parameters.Add(new NpgsqlParameter("@date_employee", Date_employment_DatePicker.SelectedDate));
                     cmd.Parameters.Add(new NpgsqlParameter("@date_dismissal", Date_deletion_DatePicker.SelectedDate));
-                    //cmd.Parameters.Add(new NpgsqlParameter("@id_organization", textBoxOrganization.Text));
-                    //cmd.Parameters.Add(new NpgsqlParameter("@ID_Division", Division_Combobox.Text));
-                    //cmd.Parameters.Add(new NpgsqlParameter("@ID_Post", Post_Combobox.Text));
-                    cmd.Parameters.Add(new NpgsqlParameter("@id_division", model.ID_Division));
                     cmd.Parameters.Add(new NpgsqlParameter("@id_organization", model.ID_Orgainzation));
+                    cmd.Parameters.Add(new NpgsqlParameter("@year_birth", DateYearBirth_employment_DatePicker.SelectedDate));
+                    cmd.Parameters.Add(new NpgsqlParameter("@id_division", model.ID_Division));
                     cmd.Parameters.Add(new NpgsqlParameter("@id_post", model.ID_Post));
-                    //cmd.Parameters.Add(new NpgsqlParameter("@Year_birth", Output_last_name_first_name_patronymic_Label));
                     cmd.ExecuteNonQuery();
 
                     MessageBox.Show("Данные успешно сохранены!", "Сообщение", MessageBoxButton.OK, MessageBoxImage.Information);
@@ -82,18 +76,15 @@ namespace ARM_Engineer.Employee
                 try
                 {
                     NpgsqlCommand cmd = new NpgsqlCommand("update \"Employee\" SET \"service_number\" = @service_number,\"name\"=@name," +
-                        "\"date_employee\"=@date_employee,\"date_dismissal\"=@date_dismissal,\"id_organization\"=@id_organization,\"id_division\"=@id_division,\"id_post\"=@id_post  WHERE \"id\" = @id",
+                        "\"date_employee\"=@date_employee,\"date_dismissal\"=@date_dismissal,\"id_organization\"=@id_organization,\"year_birth\" = @year_birth,\"id_division\"=@id_division,\"id_post\"=@id_post  WHERE \"id\" = @id",
                         DataBase.Connection());
-                  
-                    //cmd.Parameters.Add(new NpgsqlParameter("@ID_Post", Post_Combobox.Text));
-                    //cmd.Parameters.Add(new NpgsqlParameter("@ID_Organization", Organization_Combobox.Text));
-                    // cmd.Parameters.Add(new NpgsqlParameter("@Year_birth", Output_last_name_first_name_patronymic_Label));
                     cmd.Parameters.Add(new NpgsqlParameter("@id", model.ID));
                     cmd.Parameters.Add(new NpgsqlParameter("@service_number", Table_number_TextBox.Text));
                     cmd.Parameters.Add(new NpgsqlParameter("@name", Name_TextBox.Text));
                     cmd.Parameters.Add(new NpgsqlParameter("@date_employee", Date_employment_DatePicker.SelectedDate));
                     cmd.Parameters.Add(new NpgsqlParameter("@date_dismissal", Date_deletion_DatePicker.SelectedDate));
                     cmd.Parameters.Add(new NpgsqlParameter("@id_organization", model.ID_Orgainzation));
+                    cmd.Parameters.Add(new NpgsqlParameter("@year_birth", model.YearOfBirth));
                     cmd.Parameters.Add(new NpgsqlParameter("@id_division", model.ID_Division));
                     cmd.Parameters.Add(new NpgsqlParameter("@id_post", model.ID_Post));
                     cmd.ExecuteNonQuery();
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -40,9 +40,4 @@ namespace ARM_Engineer
             employee_Table_Window.Show();
         }
     }
-
-   
-
-
-
 }
