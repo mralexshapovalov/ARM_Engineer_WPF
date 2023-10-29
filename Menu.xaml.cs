11b223f1a0b7a1857c46b5e36eb015b20ab21a43
Alexandr <a.a.shapovov@bk.ru>
2023-10-29 15:31:05 +0300

    Started creating a new materials window

--- Menu.xaml.cs
+++ Menu.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ARM_Engineer.Approval_status;
+using ARM_Engineer.Employee;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -24,6 +26,31 @@ namespace ARM_Engineer
             InitializeComponent();
         }
 
+        private void Hyperlink_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+           
+
+        }
+
         
+
+       
+
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Approval_status_Windows approval_Status_Windows = new Approval_status_Windows();
+            approval_Status_Windows.Show();
+        }
+
+        private void button_Employee_Click(object sender, RoutedEventArgs e)
+        {
+            Employee_Table_Window employee_Table_Window = new Employee_Table_Window();
+            employee_Table_Window.Show();
+        }
     }
 }
