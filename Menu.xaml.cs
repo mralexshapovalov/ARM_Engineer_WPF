2312d5ee1801b740472bad9bf4875d2d07719e16
Alexandr Shapovalov <a.a.shapovalov@bk.ru>
2023-11-08 22:22:47 +0300

    Added buttons to the main menu

--- Menu.xaml.cs
+++ Menu.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using ARM_Engineer.Technic;
 
 namespace ARM_Engineer
 {
@@ -26,33 +27,10 @@ namespace ARM_Engineer
         {
             InitializeComponent();
         }
-
-        private void Hyperlink_Click(object sender, RoutedEventArgs e)
+        private void buttonObjectExploitation_Click(object sender, RoutedEventArgs e)
         {
-
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-           
-
-        }
-        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            Approval_status_Windows approval_Status_Windows = new Approval_status_Windows();
-            approval_Status_Windows.Show();
-        }
-
-        private void button_Employee_Click(object sender, RoutedEventArgs e)
-        {
-            Employee_Table_Window employee_Table_Window = new Employee_Table_Window();
-            employee_Table_Window.Show();
-        }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            AddPartWindow addPartWindow = new AddPartWindow();
-            addPartWindow.Show();
+            ObjectExploitation objectExploitation = new ObjectExploitation();
+            objectExploitation.Show();
         }
     }
 }
